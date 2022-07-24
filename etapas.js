af0ebecde46ee09cb8926c744af6d2e1677b2755
Ramon Santos <tutors4d@gmail.com>
2022-07-24 11:08:37 -0300

    3º commit

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: "Fulano da Tall",
+                partido: "ABC",
+                fotos: [
+                    {url: "38111.jpg", legenda: 'Vereador'}
+                ]
+            }, 
+            {
+                numero: 77222,
+                nome: "Beltrano Silva",
+                partido: "ABC",
+                fotos: [
+                    {url: "77222.jpg", legenda: 'Vereador'}
+                ]
+
+            }
+        ]
+    },
+    {
+        titulo: "Prefeito",
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Ciclano',
+                partido: 'BCA',
+                vice: 'Cic',
+                fotos: [
+                    {url: "99.jpg", legenda: 'Prefeito'},
+                    {url: "99_1.jpg", legenda: 'Vice-prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Zulano',
+                partido: 'FGH',
+                vice: 'Zul',
+                fotos: [
+                    {url: "84.jpg", legenda: 'Prefeito'},
+                    {url: "84_1.jpg", legenda: 'Vice-prefeito', small: true}
+                ]
+            }
+        ]
+    }
+]
